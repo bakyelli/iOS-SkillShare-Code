84b17a6305a68dfe9e8c3970ef229ee146fac49b
Joe Burgess <joe@flatironschool.com>
2013-08-18 04:59:30 -0400

    Adding Singleton Data Store

---
+++ ItemsDataStore.h
@@ -0,0 +1,15 @@
+//
+//  ItemsDataStore.h
+//  Senarai
+//
+//  Created by Joe Burgess on 8/18/13.
+//  Copyright (c) 2013 The Flatiron School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ItemsDataStore : NSObject
+
++ (ItemsDataStore *)defaultStore;
+
+@end
---
+++ ItemsDataStore.m
@@ -0,0 +1,37 @@
+//
+//  ItemsDataStore.m
+//  Senarai
+//
+//  Created by Joe Burgess on 8/18/13.
+//  Copyright (c) 2013 The Flatiron School. All rights reserved.
+//
+
+#import "ItemsDataStore.h"
+
+@implementation ItemsDataStore
+
++ (ItemsDataStore *)defaultStore
+{
+    static ItemsDataStore *defaultStore = nil;
+    if(!defaultStore)
+        defaultStore = [[super allocWithZone:nil] init];
+    
+    return defaultStore;
+}
+
++ (id)allocWithZone:(NSZone *)zone
+{
+    return [self defaultStore];
+}
+
+- (id)init
+{
+    self = [super init];
+    
+    
+    if(self) {
+    }
+    return self;
+}
+
+@end
